41aac099b02400d217b81239e3c57764c7b39a00
Vladimir Beliakov <belyakov1991@me.com>
2022-09-08 15:27:24 +0300

    Initial Commit

---
+++ Homework_tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_tableView
+//
+//  Created by Vladimir Beliakov on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
